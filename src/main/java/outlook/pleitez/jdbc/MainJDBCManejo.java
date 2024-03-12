4be27d7290c70068b9ddf3d06ac89ed9101bfadb
Walter-Pleitez <walter53torrez@outlook.com>
2024-03-11 20:59:16 -0600

    Se creo metodo Listar(), Actualizar y Eliminar en ProductoDAO - Se probo en MainJDBCManejo

--- src/main/java/outlook/pleitez/jdbc/MainJDBCManejo.java
+++ src/main/java/outlook/pleitez/jdbc/MainJDBCManejo.java
@@ -11,6 +11,14 @@ public class MainJDBCManejo {
     public static void main(String[] args) throws SQLException {
         ProductoDAO productoDAO = new ProductoDAO();
 
+        //ELIMINAR REGISTRO
+        Producto productoEliminar = new Producto(7L);
+        productoDAO.Eliminar(productoEliminar);
+
+        //MODIFICAR UN REGISTRO
+        Producto productoUpdate = new Producto(6L, "Shampo", 1.50, new Date());
+        productoDAO.actualizar(productoUpdate);
+
         //INSERTANDO UN NUEVO OBJETO DE TIPO PRODUCTO
         Producto productoNuevo = new Producto("Jabon Liquido", 1.25, new Date());
         productoDAO.insertar(productoNuevo);
--- src/main/java/outlook/pleitez/jdbc/datos/ProductoDAO.java
+++ src/main/java/outlook/pleitez/jdbc/datos/ProductoDAO.java
@@ -13,6 +13,8 @@ public class ProductoDAO {
     //variable con sentencia SQL
     private static final String sqlSelect = "SELECT id_producto, nombre, precio, fecha_registro FROM producto";
     private static final String sqlInsert = "INSERT INTO producto(nombre, precio, fecha_registro)"+" VALUES(?,?,?)";
+    private static final String sqlUpdate = "UPDATE producto SET nombre = ?, precio = ?, fecha_registro= ? WHERE id_producto = ?";
+    private static final String sqlDelete = "DELETE FROM producto WHERE id_producto = ?";
 
     //Metodo que devuelava lista de objetos de tipo producto
     public List<Producto> seleccionar() throws SQLException {
@@ -68,8 +70,58 @@ public class ProductoDAO {
             }
         }
 
+        //AGREGAR CANTIDAD DE REGISTROS MODIFICADOS
         return 0;
     }
 
+    public int actualizar(Producto producto){
+        Connection conn = null;
+        PreparedStatement pstmt = null;
+        int registros = 0;
+        try {
+            conn = getConnection();
+            pstmt = conn.prepareStatement(sqlUpdate);
+
+            pstmt.setString(1, producto.getNombreProducto());
+            pstmt.setDouble(2, producto.getPrecio());
+            pstmt.setDate(3, new Date(producto.getFechaRegistro().getTime()));
+            pstmt.setLong(4, producto.getIdProducto());
+            registros = pstmt.executeUpdate();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }finally {
+            try {
+                close(pstmt);
+                close(conn);
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+
+        return 0;
+    }
+    public int Eliminar(Producto producto){
+        Connection conn = null;
+        PreparedStatement pstmt = null;
+        int registros = 0;
+        try {
+            conn = getConnection();
+            pstmt = conn.prepareStatement(sqlDelete);
+
+            pstmt.setLong(1, producto.getIdProducto());
+            registros = pstmt.executeUpdate();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }finally {
+            try {
+                close(pstmt);
+                close(conn);
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+
+        return registros;
+    }
 
 }
