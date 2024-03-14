c73d019d35cf694a170be7dbaf0560c0f2035c24
Walter-Pleitez <walter53torrez@outlook.com>
2024-03-13 20:48:25 -0600

    Uso de Pool de conexiones

--- src/main/java/outlook/pleitez/jdbc/datos/ConexionBD.java
+++ src/main/java/outlook/pleitez/jdbc/datos/ConexionBD.java
@@ -1,5 +1,8 @@
 package outlook.pleitez.jdbc.datos;
 
+import org.apache.commons.dbcp2.BasicDataSource;
+
+import javax.sql.DataSource;
 import java.sql.*;
 import java.util.Date;
 
@@ -8,8 +11,19 @@ public class ConexionBD {
     private static final String jdbc_user = "root";
     private static final String jdbc_password = "root";
 
+    public static DataSource getDataSource(){
+        BasicDataSource bds = new BasicDataSource();
+        bds.setUrl(jdbc_url);
+        bds.setUsername(jdbc_user);
+        bds.setPassword(jdbc_password);
+        //SE DEFINE EL SIZE INICIAL PARA EL POOL
+        bds.setInitialSize(5);
+
+        return bds;
+    };
+
     public static Connection getConnection() throws SQLException {
-        return DriverManager.getConnection(jdbc_url, jdbc_user, jdbc_password);
+        return getDataSource().getConnection();
     }
     public static void close(ResultSet rs) throws SQLException {
         rs.close();
